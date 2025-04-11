71104db68992f6fcc6ef11c850ffea4ead777e3c
radha-rani-n <nradharani1998@gmail.com>
2025-04-11 16:25:25 -0400

    install required packages and dependencies

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+import { Client } from "knex";
+
+export default {
+  client: "mysql2",
+  connection: {
+    host: "127.0.0.1",
+    user: "rootroot",
+    password: "new_password",
+    database: "",
+    charset: "utf8",
+  },
+};
