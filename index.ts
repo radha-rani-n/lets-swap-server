576afd4dc6e8bb90e326aaf2f035a2587844c182
radha-rani-n <nradharani1998@gmail.com>
2025-04-15 11:40:28 -0400

    fetch from database using prisma

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import { PrismaClient } from "@prisma/client";
+
+import { PrismaClient } from "./generated/prisma";
 
 const prisma = new PrismaClient();
 
@@ -12,6 +13,14 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("This is home page");
 });
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log("Server is listening to port" + PORT);
+  try {
+    const allUsers = await prisma.post.findMany();
+    console.log(allUsers);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await prisma.$disconnect();
+  }
 });
