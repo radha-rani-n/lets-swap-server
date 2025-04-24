0093cd074d67a9838d2b0be659af14614fdf0546
radha-rani-n <nradharani1998@gmail.com>
2025-04-24 17:58:38 -0400

    add new route to get individual user posts

--- controllers/postsController.ts
+++ controllers/postsController.ts
@@ -2,9 +2,9 @@ import express from "express";
 import { PrismaClient } from "../generated/prisma";
 import { v4 as uuidv4 } from "uuid";
 import { clerkClient, getAuth, requireAuth } from "@clerk/express";
-import { error } from "console";
 const prisma = new PrismaClient();
 const router = express.Router();
+import { User } from "@clerk/express";
 
 type postProps = {
   userName: string;
@@ -14,13 +14,20 @@ type postProps = {
   locationLongitude: number;
 };
 
-const getPosts = async (req: any, res: any) => {
+const getAllPosts = async (req: any, res: any) => {
   try {
     const allPosts = await prisma.post.findMany();
     res.status(200).json(allPosts);
   } catch (e) {
     console.error(e);
   }
+};
+const getUserPosts = async (req: any, res: any) => {
+  const userId = req.auth.userId;
+  const user: User = await clerkClient.users.getUser(userId);
+  const userPosts = await prisma.post.findMany({
+    where: { userName: user.username ?? undefined },
+  });
 };
 const addPost = async (req: any, res: any) => {
   const userId = req.auth.userId;
@@ -55,13 +62,13 @@ const addPost = async (req: any, res: any) => {
   }
 };
 
-const deletePost = async (req, res) => {
-  const { userId } = getAuth(req);
+const deletePost = async (req: any, res: any) => {
+  const userId = req.auth.userId;
   const user = await clerkClient.users.getUser(userId);
   try {
     const { postId } = req.params;
     const post = await prisma.post.findUnique({
-      where: { postId: postId, userName: user.username ?? "" },
+      where: { postId: postId, userName: user.username ?? undefined },
     });
     if (!post) {
       return res.status(404).json({ error: `Post ID ${postId} not found` });
@@ -69,13 +76,14 @@ const deletePost = async (req, res) => {
     await prisma.post.delete({
       where: { userName: user.username ?? "", postId: postId },
     });
+    res.status(200).json({ Message: "Post deleted successfully" });
   } catch (e: any) {
     res.status(500).json({
       error: `Error deleting post ${req.params.postProps}: ${e.meaasage}`,
     });
   }
 };
-router.get("/", getPosts);
+router.get("/", getAllPosts);
 router.post("/", addPost);
 router.delete("/", deletePost);
 export default router;
--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(
   clerkMiddleware({
-    authorizedParties: ["http://localhost:5173"],
+    authorizedParties: ["http://localhost:5174"],
     publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
     secretKey: process.env.CLERK_SECRET_KEY,
   })
