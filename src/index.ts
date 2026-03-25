f3255a4da4a17fd9ed962e8ba6b3be6a580f6e14
radha-rani-n <nradharani1998@gmail.com>
2026-03-24 20:21:41 -0400

    rename file name from firebasetest to chats

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
-import texts from "./firebasetest";
+import texts from "./chats";
 
 import posts from "./routes/posts";
 import http from "http";
